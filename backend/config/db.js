a5c0970a5b106e934e009db96f6059c9a1c6bf0f
Dev <dev34250@gmail.com>
2026-04-09 23:58:52 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const dbConnect = () => {
   mongoose
     .connect(process.env.MONGODB_URL)
     .then(() => console.log("DB connected successfully"))
-    .catch(() => console.log("DB connection failed, try again"));
+    .catch(() => console.log("DB connection failed"));
 };
 
 module.exports = dbConnect;
